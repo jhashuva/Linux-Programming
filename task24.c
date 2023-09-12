74a020b1cced47b98b44806c0e81edecdd0fe6d1
Joshua <joshua537.nit@gmail.com>
2023-09-12 12:18:41 +0530

    Update task24.c

--- task24.c
+++ task24.c
@@ -12,37 +12,42 @@ void err_sys(const char* x)
 }
 
 void pr_exit(int status){
-   printf("Status: %d",status);
-   exit(0);
+    printf("Status: %d\n", status);
+    exit(0);
 }
 
 int main(void)
 {
-pid_t pid;
-int status;
-if ((pid = fork()) < 0)
-    err_sys("fork error");
-else if (pid == 0) /* child */
-    exit(7);
-if (wait(&status) != pid) /* wait for child */
-    err_sys("wait error");
-pr_exit(status); /* and print its status */
+    pid_t pid;
+    int status;
 
-if ((pid = fork()) < 0)
-    err_sys("fork error");
-else if (pid == 0) /* child */
-    abort(); /* generates SIGABRT */
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if (pid == 0) /* child */
+        exit(7);
 
-if (wait(&status) != pid) /* wait for child */
-    err_sys("wait error");
-pr_exit(status); /* and print its status */
+    if (wait(&status) != pid) /* wait for child */
+        err_sys("wait error");
+    pr_exit(status); /* and print its status */
 
-if ((pid = fork()) < 0)
-    err_sys("fork error");
-else if (pid == 0) /* child */
-    status /= 0; /* divide by 0 generates SIGFPE */
-if (wait(&status) != pid) /* wait for child */
-    err_sys("wait error");
-pr_exit(status); /* and print its status */
-exit(0);
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if (pid == 0) /* child */
+        abort(); /* generates SIGABRT */
+
+    if (wait(&status) != pid) /* wait for child */
+        err_sys("wait error");
+    pr_exit(status); /* and print its status */
+
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if (pid == 0) /* child */
+        status /= 0; /* divide by 0 generates SIGFPE */
+    
+    if (wait(&status) != pid) /* wait for child */
+        err_sys("wait error");
+    
+    pr_exit(status); /* and print its status */
+    
+    exit(0);
 }
